5db5bc2b5a0093d7600a375185ccce41d0693db5
desktop <deepakkhemchandani27@gmail.com>
2023-05-06 17:19:52 +0530

    working on admin login

---
+++ Solution1/Ecommerce.Admin/Controllers/AccountsController.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.Admin.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Admin.Controllers
+{
+    public class AccountsController : Controller
+    {        
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AccountsController(UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {           
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel viewModel)
+        {
+            var user = await _userManager.FindByNameAsync(viewModel.Username);
+            if (user == null) return NotFound();
+            var userRoles = await _userManager.FindByLoginAsync(viewModel.Username,viewModel.Password);
+          
+            return Ok();
+        }
+    }
+}
---
+++ Solution1/Ecommerce.Admin/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Admin.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string? Username { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+    }
+}
